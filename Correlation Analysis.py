c917e0c075222738895fcbd04fa1b5e60c08801c
andreagiral <andreagiraldop2907@gmail.com>
2025-04-22 21:42:48 -0400

    Correlation Analysis

--- Correlation Analysis.py
+++ Correlation Analysis.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 # 1. Load the dataset
 df = pd.read_csv('surv_variants_cleaned.csv')
 df.columns = df.columns.str.strip()  
